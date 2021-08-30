6276eb7e2f547bfb366b6185cd54dd253792d65d
CameliaM <89022754+Cammalon@users.noreply.github.com>
2021-08-30 10:49:09 -0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,5 @@
+var namePrompt=prompt("what is your name?");
+document.body.querySelector(".name").innerHTML=namePrompt;
+
+var colorPrompt=prompt("what is your favorite color?");
+document.body.querySelector(".color").innerHTML=colorPrompt;
